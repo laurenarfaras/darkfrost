9c99a275606a78ef15eb1db865a57d13c0e78c64
Lauren Arfaras <larfaras96@gmail.com>
2017-02-27 20:11:42 -0500

    set up the skeleton for the front end application. Added the necessary route and power up in the index.js file in order to serve those assets to the front end.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var port = process.env.PORT || 8080;
 var axios = require("axios");
 var apiKey = require("./secrets").darkskyAPIKey;
 
+server.use(express.static(__dirname + '/public'));
+
+server.get('/', function(request, response){
+  response.sendFile('index.html', {root: __dirname + '/public/html'});
+})
+
 server.get("/weather/:lat,:lon", function(request, response) {
   var url = `https://api.darksky.net/forecast/${apiKey}/${request.params.lat},${request.params.lon}`;
   axios.get(url)
