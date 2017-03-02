deca70a811559c157a31d077085d3e65ea2e8064
Lauren Arfaras <larfaras96@gmail.com>
2017-03-01 20:18:31 -0500

    created a form that dynamically pulls in weather for user-input lattitude and longitude

--- public/js/app.js
+++ public/js/app.js
@@ -7,15 +7,33 @@ var currentlyWidget = new Vue({
     icon: "partly-cloudy",
     precipProbability: 0.15,
     humidity: 0.71,
-    time: 1000000
+    time: 1000000,
+    latitude: 29.1,
+    longitude: -81.4
   },
   methods: {
-    iconUrl: function(iconString){
-      return `/images/${iconString}.png`;
+    iconName: function(iconString){
+      return `wi wi-forecast-io-${iconString}`;
+    },
+    updateWeather: function(){
+      var url = `/weather/${this.latitude},${this.longitude}`;
+      axios.get(url)
+          .then(function(response){
+            currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
+            currentlyWidget.summary = response.data.currently.summary;
+            currentlyWidget.icon = response.data.currently.icon;
+            currentlyWidget.precipProbability = response.data.currently.precipProbability;
+            currentlyWidget.humidity = response.data.currently.humidity;
+            currentlyWidget.time = response.data.currently.time;
+            console.log(response.data);
+          })
+          .catch(function(error){
+            console.log(error);
+          });
     }
   },
   created: function(){
-    axios.get('/weather/29.1,-89.4')
+    axios.get('/weather/29.1,-81.4')
         .then(function(response){
           currentlyWidget.apparentTemperature = response.data.currently.apparentTemperature;
           currentlyWidget.summary = response.data.currently.summary;
