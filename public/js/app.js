aa5498d917fcfb81e95d4bc83ea24d6b47db60f2
Lauren Arfaras <larfaras96@gmail.com>
2017-03-02 23:33:30 -0500

    added minutelyWidget and displayed the time, day of the week, precip probablity, and precip intensity for each of the minutes displayed

--- public/js/app.js
+++ public/js/app.js
@@ -143,3 +143,63 @@ var hourlyWidget = new Vue({
     this.getHourlyWeather(29.1, -84.1);
   }
 });
+
+var minutelyWidget = new Vue({
+  el: "#minutely",
+  data: {
+    summary: "it's gonna rain!",
+    icon: "cloudy",
+    apparentTemperature: 79,
+    time: 10000000,
+    precipProbability: 1,
+    precipIntensity: 2,
+    minutes: []
+  },
+  methods: {
+    getDate: function(seconds){
+      var date = new Date(seconds * 1000);
+      var month = date.getMonth();
+      var year = date.getFullYear();
+      var day = date.getDate();
+      var dayOfWeek = date.getDay();
+      if (dayOfWeek === 0){
+        dayOfWeek = "Sun";
+      } else if (dayOfWeek === 1) {
+        dayOfWeek = "Mon";
+      } else if (dayOfWeek === 2) {
+        dayOfWeek = "Tue";
+      } else if (dayOfWeek === 3) {
+        dayOfWeek = "Wed";
+      } else if (dayOfWeek === 4) {
+        dayOfWeek = "Thu";
+      } else if (dayOfWeek === 5) {
+        dayOfWeek = "Fri";
+      } else if (dayOfWeek === 6) {
+        dayOfWeek = "Sat";
+      }
+      var hour = date.getHours();
+      var minutes = date.getMinutes();
+      return `${dayOfWeek}, ${hour}:${minutes < 9 ? '0' + minutes : minutes}`;
+    },
+    // iconName: function(iconString){
+    //   return `wi wi-forecast-io-${iconString}`;
+    // },
+    getMinutelyWeather: function(lat, lon){
+      var url = `/weather/${lat},${lon}`;
+      axios.get(url)
+          .then(function(response){
+            var minutelyData = response.data.minutely;
+            this.summary = minutelyData.summary;
+            this.icon = minutelyData.icon;
+            this.minutes = minutelyData.data;
+            this.apparentTemperature = minutelyData.data.apparentTemperature;
+          }.bind(this))
+          .catch(function(err){
+            console.log(err);
+          });
+    }
+  },
+  created: function(){
+    this.getMinutelyWeather(29.1, -84.1);
+  }
+});
